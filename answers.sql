198402ae12be257aae177ed8cbd690327dbdac7d
Kelvinkimotho <kelvnkwaruingi@gmail.com>
2025-03-11 16:37:10 -0700

    my create database and drop database queries

--- answers.sql
+++ answers.sql
@@ -1,1 +1,14 @@
+-- Assignment Questions  
+-- 1. Write an SQL query to create a new database called **salesDB**. 
+-- I went ahead and created the database on mysql server by running the command below, A database with that 
+-- name is created incase it does not exist. The database has no tables by now.
+    create database salesDB; 
 
+
+ -- 2. Write an SQL query to drop (delete) the database called **demo**. 
+ -- The drop command is used to delete a database. A database must exist for it to be deleted. Below is the 
+ -- command we use to delete databases.
+    drop database demo; 
+
+-- i tried to drop the database demo which was not existing but i got an error message that the database doesn't exist. I created it
+-- and then deleted it using the command above and it succeded with no errors.
